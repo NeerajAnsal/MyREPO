35a001c7b5de79f476600173f42297b2473460cc
Neeraj <neerajansal86@gmail.com>
2018-10-07 12:25:42 +0530

    Pass

---
+++ src/main/java/TEST1/User/App.java
@@ -0,0 +1,13 @@
+package TEST1.User;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
